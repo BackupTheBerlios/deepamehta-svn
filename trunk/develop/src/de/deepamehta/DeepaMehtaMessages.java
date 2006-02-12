9cd96b1c7e100a5e1b44e5c12a1d120f11656ee6
vwegert <vwegert@ce15fd23-9a04-0410-ae48-de31837f27f4>
2006-02-12 16:28:20 +0000

    Added methods to retrieve parameterized messages; added locale handling.

--- trunk/develop/src/de/deepamehta/DeepaMehtaMessages.java
+++ trunk/develop/src/de/deepamehta/DeepaMehtaMessages.java
@@ -5,6 +5,7 @@
  */
 package de.deepamehta;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -17,7 +18,7 @@ public class DeepaMehtaMessages {
     
 	private static final String BUNDLE_NAME = "de.deepamehta.messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
 
 	/**
 	 * This class is not to be instantiated, so we make the constructor private.
@@ -36,5 +37,17 @@ public class DeepaMehtaMessages {
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
 		}
+	}
+	
+	public static String getString(String key, String var1) {
+		return getString(key).replaceAll("\\$1", var1);
+	}
+	
+	public static String getString(String key, String var1, String var2) {
+		return getString(key).replaceAll("\\$1", var1).replaceAll("\\$2", var2);
+	}
+	
+	public static String getString(String key, String var1, String var2, String var3) {
+		return getString(key).replaceAll("\\$1", var1).replaceAll("\\$2", var2).replaceAll("\\$3", var3);
 	}
 }
