b567c83f6cba07b67bd9402a3031f04000a082c7
jri <jri@ce15fd23-9a04-0410-ae48-de31837f27f4>
2006-09-29 13:48:23 +0000

    Create a branch for Enrico Schnepel to work on HSQL support

---
+++ branches/eschnepel/develop/src/de/deepamehta/TopicInitException.java
@@ -0,0 +1,16 @@
+package de.deepamehta;
+
+
+
+public class TopicInitException extends RuntimeException {
+
+    /**
+     * Constructs an <CODE>TopicInitException</CODE> with the specified 
+     * detail message. 
+     *
+     * @param   s   the detail message
+     */
+    public TopicInitException(String s) {
+		super(s);
+    }
+}
