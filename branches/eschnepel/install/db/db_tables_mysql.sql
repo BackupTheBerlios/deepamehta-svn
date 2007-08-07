6ada295f7f6f92dabf38d4489462c3210a269b80
eschnepel <eschnepel@ce15fd23-9a04-0410-ae48-de31837f27f4>
2007-08-07 08:31:33 +0000

    joinable collumns should be case-sensitive while descriptive collumns (like property values) are (the default) case-in-sensitive, everything changed to utf8

--- branches/eschnepel/install/db/db_tables_mysql.sql
+++ branches/eschnepel/install/db/db_tables_mysql.sql
@@ -2,24 +2,24 @@
 -- This script just creates the tables.
 
 CREATE TABLE Topic (
-    TypeID CHAR(40) NOT NULL,
+    TypeID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     TypeVersion INT NOT NULL,
     Version INT NOT NULL,
-    ID CHAR(40) NOT NULL,
-    Name CHAR(255) NOT NULL,
+    ID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+    Name CHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 	PRIMARY KEY (ID, Version),
     INDEX (TypeID, TypeVersion, Name)
 );
 
 CREATE TABLE Association (
-    TypeID CHAR(40) NOT NULL,
+    TypeID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     TypeVersion INT NOT NULL,
     Version INT NOT NULL,
-    ID CHAR(40) NOT NULL,
-    Name CHAR(255) NOT NULL,
-    TopicID1 CHAR(40) NOT NULL,
+    ID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+    Name CHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    TopicID1 CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     TopicVersion1 INT NOT NULL,
-    TopicID2 CHAR(40) NOT NULL,
+    TopicID2 CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     TopicVersion2 INT NOT NULL,
 	PRIMARY KEY (ID, Version),
 	INDEX (ID, Version, TypeID, TypeVersion),
@@ -28,9 +28,9 @@ CREATE TABLE Association (
 );
 
 CREATE TABLE ViewTopic (
-    ViewTopicID CHAR(40) NOT NULL,
+    ViewTopicID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
 	ViewTopicVersion INT NOT NULL,
-    TopicID CHAR(40) NOT NULL,
+    TopicID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     TopicVersion INT NOT NULL,
     x INT NOT NULL,
     y INT NOT NULL,
@@ -39,30 +39,30 @@ CREATE TABLE ViewTopic (
 );
 
 CREATE TABLE ViewAssociation (
-    ViewTopicID CHAR(40) NOT NULL,
+    ViewTopicID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
 	ViewTopicVersion INT NOT NULL,
-    AssociationID CHAR(40) NOT NULL,
+    AssociationID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     AssociationVersion INT NOT NULL,
     INDEX (ViewTopicID, ViewTopicVersion)
 );
 
 CREATE TABLE TopicProp (
-    TopicID CHAR(40) NOT NULL,
+    TopicID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     TopicVersion INT NOT NULL,
-    PropName CHAR(40) NOT NULL,
-    PropValue MEDIUMTEXT,
+    PropName CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+    PropValue MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci,
     INDEX (TopicID, TopicVersion, PropName)
 );
 
 CREATE TABLE AssociationProp (
-    AssociationID CHAR(40) NOT NULL,
+    AssociationID CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     AssociationVersion INT NOT NULL,
-    PropName CHAR(40) NOT NULL,
-    PropValue MEDIUMTEXT,
+    PropName CHAR(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+    PropValue MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci,
     INDEX (AssociationID, AssociationVersion, PropName)
 );
 
 CREATE TABLE KeyGenerator (
-    Relation CHAR(24) PRIMARY KEY NOT NULL,
+    Relation CHAR(24) CHARACTER SET utf8 COLLATE utf8_bin PRIMARY KEY NOT NULL,
     NextKey INT NOT NULL
 );
