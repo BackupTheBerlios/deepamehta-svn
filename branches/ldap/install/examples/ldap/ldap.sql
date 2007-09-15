b5a6d3ede8e356e64d15590bedc2c850faa4633b
danny <danny@ce15fd23-9a04-0410-ae48-de31837f27f4>
2007-09-15 21:34:17 +0000

    properties renamed

--- branches/ldap/install/examples/ldap/ldap.sql
+++ branches/ldap/install/examples/ldap/ldap.sql
@@ -7,18 +7,18 @@ INSERT INTO TopicProp VALUES ('t-ldapworkspace', 1, 'Name', 'LDAP');
 INSERT INTO TopicProp VALUES ('t-ldapworkspace', 1, 'Public', 'on');
 INSERT INTO TopicProp VALUES ('t-ldapworkspace', 1, 'Default', 'off');
 INSERT INTO ViewTopic VALUES ('t-workgroupmap', 1, 't-ldapworkspace', 1, 100, 100);
-INSERT INTO Association VALUES ('at-membership', 1, 1, 'a-ad-rootuserldapworkspace', '', 't-rootuser', 1, 't-ldapworkspace', 1);
+INSERT INTO Association VALUES ('at-membership', 1, 1, 'a-ldap-rootuserldapworkspace', '', 't-rootuser', 1, 't-ldapworkspace', 1);
 
 -- assign types
-INSERT INTO Association VALUES ('at-uses', 1, 1, 'a-ad-ldapworkspaceuser', '', 't-ldapworkspace', 1, 'tt-ldapuser', 1);
-INSERT INTO Association VALUES ('at-uses', 1, 1, 'a-ad-ldapworkspacegroup', '', 't-ldapworkspace', 1, 'tt-ldapgroup', 1);
-INSERT INTO AssociationProp VALUES ('a-ad-ldapworkspaceuser', 1, 'Access Permission', 'create');
-INSERT INTO AssociationProp VALUES ('a-ad-ldapworkspacegroup', 1, 'Access Permission', 'create');
+INSERT INTO Association VALUES ('at-uses', 1, 1, 'a-ldap-ldapworkspaceuser', '', 't-ldapworkspace', 1, 'tt-ldapuser', 1);
+INSERT INTO Association VALUES ('at-uses', 1, 1, 'a-ldap-ldapworkspacegroup', '', 't-ldapworkspace', 1, 'tt-ldapgroup', 1);
+INSERT INTO AssociationProp VALUES ('a-ldap-ldapworkspaceuser', 1, 'Access Permission', 'create');
+INSERT INTO AssociationProp VALUES ('a-ldap-ldapworkspacegroup', 1, 'Access Permission', 'create');
 
 -- workspace topicmap
 INSERT INTO Topic VALUES ('tt-topicmap', 1, 1, 't-ldaptopicmap', 'LDAP');
 INSERT INTO TopicProp VALUES ('t-ldaptopicmap', 1, 'Name', 'LDAP');
-INSERT INTO Association VALUES ('at-aggregation', 1, 1, 'a-ad-1', '', 't-ldapworkspace', 1, 't-ldaptopicmap', 1);
+INSERT INTO Association VALUES ('at-aggregation', 1, 1, 'a-ldap-1', '', 't-ldapworkspace', 1, 't-ldaptopicmap', 1);
 
 -- chat
 INSERT INTO Topic VALUES ('tt-chatboard', 1, 1, 't-ldapchat', 'LDAP Chats');
@@ -60,27 +60,27 @@ INSERT INTO TopicProp VALUES ('tt-ldapgroupcontainer', 1, 'Icon', 'ldapgroupcont
 INSERT INTO TopicProp VALUES ('tt-ldapgroupcontainer', 1, 'Custom Implementation', 'de.deepamehta.topics.LdapGroupContainerTopic');
 
 -- property associations
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-cngroup', '', 'tt-ldapgroup', 1, 'pp-cn', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-descriptiongroup', '', 'tt-ldapgroup', 1, 'pp-description', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-descriptionuser', '', 'tt-ldapuser', 1, 'pp-description', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-givennameuser', '', 'tt-ldapuser', 1, 'pp-givenname', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-membergroup', '', 'tt-ldapgroup', 1, 'pp-member', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-memberofuser', '', 'tt-ldapuser', 1, 'pp-memberof', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-ougroup', '', 'tt-ldapgroup', 1, 'pp-ou', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-ougroupcontainer', '', 'tt-ldapgroupcontainer', 1, 'pp-ou', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-snuser', '', 'tt-ldapuser', 1, 'pp-sn', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-uiduser', '', 'tt-ldapuser', 1, 'pp-uid', 1);
-INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ad-uidusercontainer', '', 'tt-ldapusercontainer', 1, 'pp-uid', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-cngroup', '', 'tt-ldapgroup', 1, 'pp-cn', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-descriptiongroup', '', 'tt-ldapgroup', 1, 'pp-description', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-descriptionuser', '', 'tt-ldapuser', 1, 'pp-description', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-givennameuser', '', 'tt-ldapuser', 1, 'pp-givenname', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-membergroup', '', 'tt-ldapgroup', 1, 'pp-member', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-memberofuser', '', 'tt-ldapuser', 1, 'pp-memberof', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-ougroup', '', 'tt-ldapgroup', 1, 'pp-ou', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-ougroupcontainer', '', 'tt-ldapgroupcontainer', 1, 'pp-ou', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-snuser', '', 'tt-ldapuser', 1, 'pp-sn', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-uiduser', '', 'tt-ldapuser', 1, 'pp-uid', 1);
+INSERT INTO Association VALUES ('at-composition', 1, 1, 'a-ldap-uidusercontainer', '', 'tt-ldapusercontainer', 1, 'pp-uid', 1);
 
 -- associations between document types and container types
-INSERT INTO Association VALUES ('at-aggregation', 1, 1, 'a-ad-user-usercontassoc', '', 'tt-ldapusercontainer', 1, 'tt-ldapuser', 1);
-INSERT INTO Association VALUES ('at-aggregation', 1, 1, 'a-ad-group-groupcontassoc', '', 'tt-ldapgroupcontainer', 1, 'tt-ldapgroup', 1);
+INSERT INTO Association VALUES ('at-aggregation', 1, 1, 'a-ldap-user-usercontassoc', '', 'tt-ldapusercontainer', 1, 'tt-ldapuser', 1);
+INSERT INTO Association VALUES ('at-aggregation', 1, 1, 'a-ldap-group-groupcontassoc', '', 'tt-ldapgroupcontainer', 1, 'tt-ldapgroup', 1);
 
 -- associations to LDAP datasource
-INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ad-userassoc', '', 'tt-ldapuser', 1, 't-ldapdatasource', 1);
-INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ad-groupassoc', '', 'tt-ldapgroup', 1, 't-ldapdatasource', 1);
-INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ad-usercontassoc', '', 'tt-ldapusercontainer', 1, 't-ldapdatasource', 1);
-INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ad-groupcontassoc', '', 'tt-ldapgroupcontainer', 1, 't-ldapdatasource', 1);
+INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ldap-userassoc', '', 'tt-ldapuser', 1, 't-ldapdatasource', 1);
+INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ldap-groupassoc', '', 'tt-ldapgroup', 1, 't-ldapdatasource', 1);
+INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ldap-usercontassoc', '', 'tt-ldapusercontainer', 1, 't-ldapdatasource', 1);
+INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ldap-groupcontassoc', '', 'tt-ldapgroupcontainer', 1, 't-ldapdatasource', 1);
 
 -------------------------
 --- Association Types ---
@@ -141,7 +141,7 @@ INSERT INTO TopicProp VALUES ('t-ldapdatasource', 1, 'Driver', 'com.sun.jndi.lda
 INSERT INTO Topic VALUES ('tt-webpage', 1, 1, 't-openldapwebpage', 'OpenLDAP');
 INSERT INTO TopicProp VALUES ('t-openldapwebpage', 1, 'Name', 'OpenLDAP');
 INSERT INTO TopicProp VALUES ('t-openldapwebpage', 1, 'URL', 'http://www.openldap.org/');
-INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ad-openldapdatasource', '', 't-openldapwebpage', 1, 't-ldapdatasource', 1);
+INSERT INTO Association VALUES ('at-association', 1, 1, 'a-ldap-openldapdatasource', '', 't-openldapwebpage', 1, 't-ldapdatasource', 1);
 
 --------------------
 --- Workpad View ---
@@ -153,11 +153,11 @@ INSERT INTO ViewTopic VALUES ('t-ldaptopicmap', 1, 't-ldapmap', 1, 300, 100);
 
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 't-openldapwebpage', 1, 300, 50);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 't-ldapdatasource', 1, 300, 100);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-groupassoc', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-groupcontassoc', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-openldapdatasource', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-userassoc', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-usercontassoc', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-groupassoc', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-groupcontassoc', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-openldapdatasource', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-userassoc', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-usercontassoc', 1);
 
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'at-ldapmember', 1, 100, 300);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'pp-ou', 1, 100, 150);
@@ -165,11 +165,11 @@ INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'pp-cn', 1, 100, 200);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'pp-member', 1, 100, 250);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'tt-ldapgroup', 1, 225, 200);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'tt-ldapgroupcontainer', 1, 225, 150);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-cngroup', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-group-groupcontassoc', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-membergroup', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-ougroup', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-ougroupcontainer', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-cngroup', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-group-groupcontassoc', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-membergroup', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-ougroup', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-ougroupcontainer', 1);
 
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'at-ldapmemberof', 1, 500, 350);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'pp-givenname', 1, 500, 200);
@@ -178,15 +178,15 @@ INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'pp-memberof', 1, 500, 300);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'pp-uid', 1, 500, 150);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'tt-ldapuser', 1, 375, 200);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 'tt-ldapusercontainer', 1, 375, 150);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-givennameuser', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-memberofuser', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-snuser', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-user-usercontassoc', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-uiduser', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-uidusercontainer', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-givennameuser', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-memberofuser', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-snuser', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-user-usercontassoc', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-uiduser', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-uidusercontainer', 1);
 
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 't-ldapworkspace', 1, 300, 250);
 INSERT INTO ViewTopic VALUES ('t-ldapmap', 1, 't-rootuser', 1, 300, 300);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-ldapworkspacegroup', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-ldapworkspaceuser', 1);
-INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ad-rootuserldapworkspace', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-ldapworkspacegroup', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-ldapworkspaceuser', 1);
+INSERT INTO ViewAssociation VALUES ('t-ldapmap', 1, 'a-ldap-rootuserldapworkspace', 1);
